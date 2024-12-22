3cd3380752e4950bdae8e8746cdf934950b3a16e
Wesley Oliveira <wesleyumsocaminho@gmail.com>
2024-12-22 10:59:37 -0300

    feature: novo endpoint, validação com schema, novos testes

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,26 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
 @app.get('/')
-def read_root():
+def read_root(status_code=HTTPStatus.OK, response_model=Message):
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/html', response_class=HTMLResponse)
+def read_root_html():
+    return """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -11,3 +11,12 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK  # assert (validacao)
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_root_deve_retornar_ola_mundo_em_html():
+    client = TestClient(app)
+
+    response = client.get('/html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo </h1>' in response.text
